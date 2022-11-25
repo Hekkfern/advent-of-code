52eaf5de17ea31cd1ccf1087a3371c56292f899d
Hector Fernandez <hector.fernandez@king.com>
2022-11-25 22:50:02 +0100

    .

--- 2021/01/aoc_2021_01/main.cpp
+++ 2021/01/aoc_2021_01/main.cpp
@@ -1,6 +1,32 @@
+#include <fstream>
+#include <string>
+#include <vector>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+std::vector<std::string> ReadLinesFromFile(const char* filename) {
+	std::ifstream stream(filename);
+
+	std::vector<std::string> all_lines;
+	std::string line;
+	while (std::getline(stream, line)) {
+		all_lines.emplace_back(std::move(line));
+	}
+	stream.close();
+	return all_lines;
+}
+
+int main()
+{
+	const auto inputValues = ReadLinesFromFile("input.csv");
+	int counter = 0;
+	for (int i = 1; i < inputValues.size(); ++i)
+	{
+		if (inputValues[i] > inputValues[i-1])
+		{
+			++counter;
+		}
+	}
+	std::cout << counter << std::endl;
+
+	return 0;
 }
