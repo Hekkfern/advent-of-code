813b3438bca7078dfc158921fc2a496568e06c5f
Hector Fernandez <hector.fernandez@king.com>
2022-12-25 21:40:33 +0100

    .

--- cmake/cpp_standard.cmake
+++ cmake/cpp_standard.cmake
@@ -1,7 +1,7 @@
 macro(set_cpp_standard target_name)
     # C++20 standard
-    target_compile_features(${target_name} PRIVATE cxx_std_20)
     set_target_properties(${target_name} PROPERTIES
+        CXX_STANDARD 20
         CXX_STANDARD_REQUIRED ON
         CXX_EXTENSIONS OFF
     )
