a6d146515021539daa3827acd270e60fc143f793
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2022-12-27 18:55:06 +0100

    .

--- puzzles/2021/01/solutions.cpp
+++ puzzles/2021/01/solutions.cpp
@@ -1,9 +1,19 @@
 #include "solutions.hpp"
 
+#include <utils/InputUtils.hpp>
+
 namespace aoc_2021_01 {
 
 std::string solvePart1() {
-    return "";
+    const auto inputList = utils::InputUtils::readListOfNumbers("input.txt");
+    int64_t counter = 0;
+    for (size_t i = 1; i < inputList.size() ; ++i) {
+        if (inputList[i] > inputList[i-1])
+        {
+            ++counter;
+        }
+    }
+    return std::to_string(counter);;
 }
 
 std::string solvePart2(){
