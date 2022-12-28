f1e9ee2f8292a891bd110a0e797ad8f3ac31f461
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2022-12-28 22:26:10 +0100

    .

--- scripts/build.py
+++ scripts/build.py
@@ -1,2 +1,5 @@
 cmake -B [build directory] -S . -DCMAKE_TOOLCHAIN_FILE=[path to vcpkg]/scripts/buildsystems/vcpkg.cmake
 cmake --build [build directory]
+
+
+./extern/vcpkg/vcpkg --triplet=x64-windows
