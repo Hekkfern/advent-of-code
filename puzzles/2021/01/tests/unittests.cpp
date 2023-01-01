485dd8135c4a7fb4e17b33ee34451832c2e550c4
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2023-01-01 19:53:22 +0100

    .

--- puzzles/2021/01/tests/unittests.cpp
+++ puzzles/2021/01/tests/unittests.cpp
@@ -12,3 +12,23 @@ TEST_CASE("[2021_01] Use example input", "[2021, 2021_01]")
 
     REQUIRE(mySolution == expectedSolution);
 }
+
+TEST_CASE("[2021_01] Use real input for Part 1", "[2021, 2021_01, part1, 2021_01_part1]")
+{
+    constexpr auto inputFile {"input.txt"};
+
+    std::string mySolution = aoc_2021_01::solvePart1(inputFile);
+    std::string expectedSolution = utils::FileUtils::readFirstLine("solution1.txt").value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE("[2021_01] Use real input for Part 2", "[2021, 2021_01, part1, 2021_01_part2]")
+{
+    constexpr auto inputFile {"input.txt"};
+
+    std::string mySolution = aoc_2021_01::solvePart2(inputFile);
+    std::string expectedSolution = utils::FileUtils::readFirstLine("solution2.txt").value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
