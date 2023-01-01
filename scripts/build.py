ce983298a1a162e8c6dd5047569b8041ad5d23df
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2023-01-01 13:35:57 +0100

    .

--- scripts/build.py
+++ scripts/build.py
@@ -5,6 +5,7 @@ import sys
 import shutil
 import pathlib
 import git
+import colorama
 
 
 def __get_root_project_path() -> pathlib.Path:
@@ -39,15 +40,15 @@ def __get_input_parameters():
         prog='AoC Builder',
         description='Assistant software to operate this project in a easier way')
     subparsers = parser.add_subparsers(dest="subcommand", required=True)
-    parser_build = subparsers.add_parser('build')
-    parser_generate = subparsers.add_parser('generate')
-    parser_update = subparsers.add_parser('update')
+    parser_build = subparsers.add_parser('build', help="Cleans, generates and compiles the project.")
+    parser_generate = subparsers.add_parser('generate', help="Generates the project with CMake.")
+    parser_generate.add_argument("-d", "--debug", action="store_true", help="Sets the debug flags into the compilation environment.")
     parser_generate.add_argument(
-        "--unit-tests", action="store_true", help="Generates the unit-test project too.")
-    parser_compile = subparsers.add_parser('compile')
-    parser_fetch = subparsers.add_parser('fetch')
-    parser_clean = subparsers.add_parser('clean')
-    parser_addday = subparsers.add_parser('add_day')
+        "-ut", "--unit-tests", action="store_true", help="Generates the unit-test project too.")
+    parser_compile = subparsers.add_parser('compile', help="Compiles the CMake project if it was previously generated.")
+    parser_update = subparsers.add_parser('update', help="Downloads the latest version of this project and updates all its dependencies.")
+    parser_clean = subparsers.add_parser('clean', help="Deletes all the local data stored in the project.")
+    parser_addday = subparsers.add_parser('add_day', help='Set up the project to add a new "Advent Of Code" puzzle.')
     parser_addday.add_argument(
         "-y", "--year", type=int, required=True, help="Selects the year (format XXXX, as for instance, 2023) of the puzzle to generate.")
     parser_addday.add_argument(
@@ -57,9 +58,28 @@ def __get_input_parameters():
 
 
 def main():
+    colorama.init()
+
     __check_min_python_version()
 
     args = __get_input_parameters()
+    if args.subcommand == "build":
+        pass
+    elif args.subcommand == "update":
+        pass
+    elif args.subcommand == "generate":
+        pass
+    elif args.subcommand == "compile":
+        pass
+    elif args.subcommand == "update":
+        pass
+    elif args.subcommand == "clean":
+        pass
+    elif args.subcommand == "add_day":
+        pass
+    else:
+        print("This script requires Python version 3.7")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
