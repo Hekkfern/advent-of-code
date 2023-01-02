d2c355ca2c211e6453443153275c8b8b3196bedb
Hector Fernandez <hector.fernandez@king.com>
2023-01-02 08:28:13 +0100

    .

--- puzzles/2021/01/tests/unittests.cpp
+++ puzzles/2021/01/tests/unittests.cpp
@@ -3,12 +3,22 @@
 #include <catch.hpp>
 #include <utils/FileUtils.hpp>
 
-TEST_CASE("[2021_01] Use example input", "[2021, 2021_01]")
+TEST_CASE("[2021_01] Use example input for Part 1", "[2021, 2021_01, part1, 2021_01_part1]")
 {
     constexpr auto inputFile {"input_test.txt"};
 
     std::string mySolution = aoc_2021_01::solvePart1(inputFile);
-    std::string expectedSolution = utils::FileUtils::readFirstLine("solution_test.txt").value();
+    std::string expectedSolution = utils::FileUtils::readFirstLine("solution1_test.txt").value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE("[2021_01] Use example input for Part 2", "[2021, 2021_01, part2, 2021_01_part2]")
+{
+    constexpr auto inputFile {"input_test.txt"};
+
+    std::string mySolution = aoc_2021_01::solvePart2(inputFile);
+    std::string expectedSolution = utils::FileUtils::readFirstLine("solution2_test.txt").value();
 
     REQUIRE(mySolution == expectedSolution);
 }
