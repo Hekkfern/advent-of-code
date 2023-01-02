dc5d1c4695067d12918d0be07e2a73c53f34c01a
Hector Fernandez <hector.fernandez@king.com>
2023-01-02 17:23:06 +0100

    [WIP] part 2

--- puzzles/2021/01/sources/solutions.cpp
+++ puzzles/2021/01/sources/solutions.cpp
@@ -1,5 +1,6 @@
 #include "solutions.hpp"
 
+#include <array>
 #include <fstream>
 #include <utils/StringUtils.hpp>
 
@@ -27,8 +28,30 @@ std::string solvePart1(const std::string& filename)
 
 std::string solvePart2(const std::string& filename)
 {
-    (void)filename;
-    return "";
+    constexpr int32_t SlidingWindowSize{ 3 };
+
+    std::ifstream stream{ filename };
+    std::string line;
+    int64_t increaseCounter = 0;
+    int64_t numLines = 0;
+    std::array<int64_t, SlidingWindowSize + 1> sums{};
+    int32_t slidingWindowIndex = 0;
+
+    while (std::getline(stream, line)) {
+        int64_t value = utils::StringUtils::toNumber<int64_t>(line);
+        // add the new value in every sum
+        for 
+        sums[slidingWindowIndex] += value;
+        numLines++;
+        if (numLines >= SlidingWindowSize) {
+            if ()
+                ++increaseCounter;
+            // clean
+            sums[slidingWindowIndex] = 0;
+        }
+    }
+
+    return std::to_string(increaseCounter);
 }
 
 } // namespace aoc_2021_01
