837c201429500cfe7561fe10ed443c4cac6955f0
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2023-01-07 14:10:29 +0100

    .

--- cmake/top_level_project.cmake
+++ cmake/top_level_project.cmake
@@ -1,5 +1,5 @@
 macro(is_top_level_project)
   if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-    message(FATAL_ERROR “In-source build detected!”)
+    message(FATAL_ERROR "In-source builds are not supported.")
   endif()
 endmacro()
