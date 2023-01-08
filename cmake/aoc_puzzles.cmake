1aa869ae72484490b0997da0f6c9ea982d2cab54
Hector Fernandez <hecgtor.fernandez@king.com>
2023-01-08 22:52:16 +0100

    .

--- cmake/aoc_puzzles.cmake
+++ cmake/aoc_puzzles.cmake
@@ -8,3 +8,13 @@ macro(setup_puzzle target_name)
     set_compiler_flags(${target_name})
     set_vectorization_flags(${target_name})
 endmacro()
+
+macro(setup_tests target_name)
+    set_cpp_standard(${target_name})
+    set_compiler_flags(${target_name})
+    set_vectorization_flags(${target_name})
+
+    include(CTest)
+    include(Catch)
+    catch_discover_tests(${_EXEC_NAME})
+endmacro()
