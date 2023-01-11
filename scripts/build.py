20b3fd7adbcb299a43aa83d63daaba9fb616e372
Hector Fernandez <hecgtor.fernandez@king.com>
2023-01-11 18:56:43 +0100

    .

--- scripts/build.py
+++ scripts/build.py
@@ -116,7 +116,7 @@ def __compile_project():
     if not (out_preset_path / "CMakeCache.txt").is_file():
         __abort_execution("Output folder doesn't exist. Generate the project first.")
     # run CMake
-    command: str = f'cmake --build {out_preset_path.absolute()} --target all'
+    command: str = f'cmake --build --preset {preset} --target all'
     execute_program(command)
     print()  # add empty line in stdout
 
