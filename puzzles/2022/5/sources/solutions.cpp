7e35eb9adbd3ae7c1f3cf2a66c8cdd4590f1d526
Hector Fernandez <hecgtor.fernandez@king.com>
2023-01-15 13:18:37 +0100

    .

--- puzzles/2022/5/sources/solutions.cpp
+++ puzzles/2022/5/sources/solutions.cpp
@@ -1,7 +1,5 @@
 #include "solutions.hpp"
 
-#include "stacks.h"
-
 #include <algorithm>
 #include <fstream>
 #include <range/v3/all.hpp>
@@ -9,6 +7,19 @@
 #include <vector>
 
 namespace aoc_2022_5 {
+
+// ---------- Private types ----------
+
+using StackId = uint32_t;
+
+struct CraneInstruction {
+    uint32_t mNumCrates;
+    StackId mOriginStackIndex;
+    StackId mDestinationStackIndex;
+};
+
+using CrateStack = std::vector<char>; // where index 0 means the bottom of the
+                                      // stack
 
 // ---------- Private Methods ----------
 
@@ -76,6 +87,40 @@ std::string generateResult(const std::vector<CrateStack>& crateStackList)
     return result;
 }
 
+void executeCrateMover9000Instruction(
+    std::vector<CrateStack>& crateStackList,
+    const CraneInstruction& craneInstruction)
+{
+    auto& originStack = crateStackList[craneInstruction.mOriginStackIndex];
+    // view of the last elements of the stack, in reverse order
+    auto originStackView = originStack | ranges::views::reverse
+        | ranges::views::take(craneInstruction.mNumCrates);
+    // copy to the end of the other stack
+    ranges::copy(
+        originStackView,
+        std::back_inserter(
+            crateStackList[craneInstruction.mDestinationStackIndex]));
+    // remove the last elements of the origin stack
+    originStack.resize(originStack.size() - craneInstruction.mNumCrates);
+}
+
+void executeCrateMover9001Instruction(
+    std::vector<CrateStack>& crateStackList,
+    const CraneInstruction& craneInstruction)
+{
+    auto& originStack = crateStackList[craneInstruction.mOriginStackIndex];
+    // view of the last elements of the stack, in reverse order
+    auto originStackView = originStack
+        | ranges::views::take(craneInstruction.mNumCrates);
+    // copy to the end of the other stack
+    ranges::copy(
+        originStackView,
+        std::back_inserter(
+            crateStackList[craneInstruction.mDestinationStackIndex]));
+    // remove the last elements of the origin stack
+    originStack.resize(originStack.size() - craneInstruction.mNumCrates);
+}
+
 // ---------- End of Private Methods ----------
 
 // ---------- Public Methods ----------
@@ -87,15 +132,21 @@ std::string solvePart1(const std::string& filename)
     std::string line;
     while (std::getline(fileStream, line)) {
         const CraneInstruction craneInstruction = parseInputInstruction(line);
-        executeCrateInstruction(crateStackList, craneInstruction);
+        executeCrateMover9000Instruction(crateStackList, craneInstruction);
     }
     return generateResult(crateStackList);
 }
 
 std::string solvePart2(const std::string& filename)
 {
-    (void)filename;
-    return "";
+    std::ifstream fileStream{ filename };
+    std::vector<CrateStack> crateStackList = parseInputStacks(fileStream);
+    std::string line;
+    while (std::getline(fileStream, line)) {
+        const CraneInstruction craneInstruction = parseInputInstruction(line);
+        executeCrateMover9001Instruction(crateStackList, craneInstruction);
+    }
+    return generateResult(crateStackList);
 }
 
 // ---------- End of Private Methods ----------
