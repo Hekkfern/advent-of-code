8ce27ed75f400a632dd116405cdab66cd2bd1382
Hector Fernandez <hector.fernandez@king.com>
2023-02-06 18:03:25 +0100

    .

--- puzzles/2022/11/sources/solutions.cpp
+++ puzzles/2022/11/sources/solutions.cpp
@@ -1,10 +1,25 @@
 #include "solutions.hpp"
 
+#include <array>
 #include <fstream>
+#include <regex>
 
 namespace aoc_2022_11 {
 
 // ---------- Private Methods ----------
+
+void parseInput(const std::string& filename){
+    constexpr uint8_t InputLinesPerMonkey{6U};
+    std::ifstream inputFile{ filename };
+    std::array<std::string,InputLinesPerMonkey> lines;
+
+    for (uint8_t i = 0U; i <InputLinesPerMonkey; ++i){
+        std::getline(inputFile, lines.at(i));
+    }
+
+
+}
+
 
 // ---------- End of Private Methods ----------
 
