4f75b553712e2998c79cf20fc05612e737d18d05
Hector Fernandez <hecgtor.fernandez@king.com>
2023-03-05 22:50:51 -0500

    .

--- puzzles/2022/2/sources/solutions.cpp
+++ puzzles/2022/2/sources/solutions.cpp
@@ -1,93 +1,73 @@
 #include "solutions.hpp"
 
 #include <fstream>
+#include <unordered_map>
 
 namespace aoc_2022_2 {
 
 // ---------- Private Methods ----------
 
-/**
- * @brief      This class describes a shape.
- */
-enum class Shape { Rock, Paper, Scissors };
+// A hash function used to hash a pair of any kind
+struct HashPair {
+    template <class T1, class T2>
+    size_t operator()(const std::pair<T1, T2>& p) const
+    {
+        auto hash1{ std::hash<T1>{}(p.first) };
+        auto hash2{ std::hash<T2>{}(p.second) };
 
-/**
- * @brief      This class describes a match result.
- */
-enum class MatchResult { Lose, Draw, Win };
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;
+        }
 
-/**
- * @brief      Converts a shape character into its correcponding enum value.
- *
- * @param[in]  str   Character to convert to.
- *
- * @return     Enum value corresponding to the converted character.
- */
-Shape convertStringToShape(char str)
-{
-    if (str == 'A' || str == 'X') {
-        return Shape::Rock;
-    } else if (str == 'B' || str == 'Y') {
-        return Shape::Paper;
-    } else if (str == 'C' || str == 'Z') {
-        return Shape::Scissors;
-    } else {
-        throw std::invalid_argument("Invalid character.");
+        // If hash1 == hash2, their XOR is zero.
+        return hash1;
     }
-}
+};
 
 constexpr uint32_t PointsPerUsedRock{ 1U };
 constexpr uint32_t PointsPerUsedPaper{ 2U };
 constexpr uint32_t PointsPerUsedScissors{ 3U };
-
-/**
- * @brief      Calculates the points earned by uing a shape in a match.
- *
- * @param[in]  yourShape  The shape you use during the match.
- *
- * @return     The points earned.
- */
-uint32_t calculatePointsOfUsedShape(const Shape yourShape)
-{
-    switch (yourShape) {
-    case Shape::Rock:
-        return PointsPerUsedRock;
-    case Shape::Paper:
-        return PointsPerUsedPaper;
-    case Shape::Scissors:
-        return PointsPerUsedScissors;
-    default:
-        throw std::invalid_argument("Invalid enum value.");
-    }
-}
-
 constexpr uint32_t PointsPerLost{ 0U };
 constexpr uint32_t PointsPerDraw{ 3U };
 constexpr uint32_t PointsPerWin{ 6U };
 
+constexpr char OpponentRock{ 'A' };
+constexpr char OpponentPaper{ 'B' };
+constexpr char OpponentScissors{ 'C' };
+constexpr char PlayerRock{ 'X' };
+constexpr char PlayerPaper{ 'Y' };
+constexpr char PlayerScissors{ 'Z' };
+constexpr char MatchLost{ 'X' };
+constexpr char MatchDraw{ 'Y' };
+constexpr char MatchWin{ 'Z' };
+
+std::unordered_map<std::pair<char, char>, uint32_t, HashPair> PointsPerShapes{
+    { { OpponentRock, PlayerRock }, PointsPerUsedRock + PointsPerDraw },
+    { { OpponentRock, PlayerPaper }, PointsPerUsedPaper + PointsPerWin },
+    { { OpponentRock, PlayerScissors }, PointsPerUsedScissors + PointsPerLost },
+    { { OpponentPaper, PlayerRock }, PointsPerUsedRock + PointsPerLost },
+    { { OpponentPaper, PlayerPaper }, PointsPerUsedPaper + PointsPerDraw },
+    { { OpponentPaper, PlayerScissors }, PointsPerUsedScissors + PointsPerWin },
+    { { OpponentScissors, PlayerRock }, PointsPerUsedRock + PointsPerWin },
+    { { OpponentScissors, PlayerPaper }, PointsPerUsedPaper + PointsPerLost },
+    { { OpponentScissors, PlayerScissors },
+      PointsPerUsedScissors + PointsPerDraw }
+};
+
 /**
- * @brief      Calculates the points earned as the result of the match.
- *
- * @param[in]  yourShape      The shape you use during the match.
- * @param[in]  opponentShape  The shape your opponent uses during the match.
- *
- * @return     The points earned.
+ * @brief Look-up table which links the opponent shape and the result of the match, to the shape that the player must use to reach the said result.
  */
-uint32_t calculatePointsPerMatch(
-    const Shape yourShape,
-    const Shape opponentShape)
-{
-    if (yourShape == opponentShape) {
-        return PointsPerDraw;
-    } else if (
-        (yourShape == Shape::Rock && opponentShape == Shape::Scissors)
-        || (yourShape == Shape::Paper && opponentShape == Shape::Rock)
-        || (yourShape == Shape::Scissors && opponentShape == Shape::Paper)) {
-        return PointsPerWin;
-    } else {
-        return PointsPerLost;
-    }
-}
+std::unordered_map<std::pair<char, char>, char, HashPair> ResultConversion{
+    { { OpponentRock, MatchLost }, PlayerScissors },
+    { { OpponentRock, MatchDraw }, PlayerRock },
+    { { OpponentRock, MatchWin }, PlayerPaper },
+    { { OpponentPaper, MatchLost }, PlayerRock },
+    { { OpponentPaper, MatchDraw }, PlayerPaper },
+    { { OpponentPaper, MatchWin }, PlayerScissors },
+    { { OpponentScissors, MatchLost }, PlayerPaper },
+    { { OpponentScissors, MatchDraw }, PlayerScissors },
+    { { OpponentScissors, MatchWin }, PlayerRock }
+};
 
 /**
  * @brief      Calculates the total round score, as result of adding the points
@@ -99,74 +79,11 @@ uint32_t calculatePointsPerMatch(
  *
  * @return     The total points of the round.
  */
-uint32_t calculateRoundScore(const Shape yourShape, const Shape opponentShape)
+uint32_t calculateRoundScoreFromShapes(
+    const char yourShape,
+    const char opponentShape)
 {
-    return calculatePointsOfUsedShape(yourShape)
-        + calculatePointsPerMatch(yourShape, opponentShape);
-}
-
-/**
- * @brief      Converts a match result character into its correcponding enum
- *             value.
- *
- * @param[in]  str   Character to convert to.
- *
- * @return     Enum value corresponding to the converted character.
- */
-MatchResult convertStringToMatchResult(char str)
-{
-    if (str == 'X') {
-        return MatchResult::Lose;
-    } else if (str == 'Y') {
-        return MatchResult::Draw;
-    } else if (str == 'Z') {
-        return MatchResult::Win;
-    } else {
-        throw std::invalid_argument("Invalid character.");
-    }
-}
-
-/**
- * @brief      Determines the shape you should use in order to achieve the
- *             selected result during the match.
- *
- * @param[in]  matchResult    The match result to achieve.
- * @param[in]  opponentShape  The shape your opponent uses during the match.
- *
- * @return     The shape you should use.
- */
-Shape selectShapeBasedOnMatchResult(
-    const MatchResult matchResult,
-    const Shape opponentShape)
-{
-    switch (matchResult) {
-    case MatchResult::Lose:
-        switch (opponentShape) {
-        case Shape::Rock:
-            return Shape::Scissors;
-        case Shape::Paper:
-            return Shape::Rock;
-        case Shape::Scissors:
-            return Shape::Paper;
-        default:
-            throw std::invalid_argument("Invalid enum.");
-        }
-    case MatchResult::Draw:
-        return opponentShape;
-    case MatchResult::Win:
-        switch (opponentShape) {
-        case Shape::Rock:
-            return Shape::Paper;
-        case Shape::Paper:
-            return Shape::Scissors;
-        case Shape::Scissors:
-            return Shape::Rock;
-        default:
-            throw std::invalid_argument("Invalid enum.");
-        }
-    default:
-        throw std::invalid_argument("Invalid enum.");
-    }
+    return PointsPerShapes.at(std::make_pair(opponentShape, yourShape));
 }
 
 /**
@@ -179,13 +96,13 @@ Shape selectShapeBasedOnMatchResult(
  *
  * @return     The total points of the round.
  */
-uint32_t calculateRoundScore(
-    const MatchResult matchResult,
-    const Shape opponentShape)
+uint32_t calculateRoundScoreFromResult(
+    const char matchResult,
+    const char opponentShape)
 {
-    const Shape yourShape = selectShapeBasedOnMatchResult(
-        matchResult, opponentShape);
-    return calculateRoundScore(yourShape, opponentShape);
+    return PointsPerShapes.at(std::make_pair(
+        opponentShape,
+        ResultConversion.at(std::make_pair(opponentShape, matchResult))));
 }
 
 // ---------- End of Private Methods ----------
@@ -196,12 +113,10 @@ std::string solvePart1(const std::string& filename)
 {
     std::ifstream stream{ filename };
     std::string line;
-    uint32_t totalScore = 0U;
+    uint32_t totalScore{ 0U };
 
     while (std::getline(stream, line)) {
-        const Shape opponentShape = convertStringToShape(line[0]);
-        const Shape yourShape = convertStringToShape(line[2]);
-        totalScore += calculateRoundScore(yourShape, opponentShape);
+        totalScore += calculateRoundScoreFromShapes(line[2], line[0]);
     }
 
     return std::to_string(totalScore);
@@ -211,12 +126,10 @@ std::string solvePart2(const std::string& filename)
 {
     std::ifstream stream{ filename };
     std::string line;
-    uint32_t totalScore = 0U;
+    uint32_t totalScore{ 0U };
 
     while (std::getline(stream, line)) {
-        const Shape opponentShape = convertStringToShape(line[0]);
-        const MatchResult matchResult = convertStringToMatchResult(line[2]);
-        totalScore += calculateRoundScore(matchResult, opponentShape);
+        totalScore += calculateRoundScoreFromResult(line[2], line[0]);
     }
 
     return std::to_string(totalScore);
