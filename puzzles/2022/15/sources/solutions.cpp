da0f77f30e64b3bf65a5458f2fb215be824fdeec
Hector Fernandez <hector.fernandez@king.com>
2023-03-10 07:52:23 +0100

    .

--- puzzles/2022/15/sources/solutions.cpp
+++ puzzles/2022/15/sources/solutions.cpp
@@ -1,10 +1,31 @@
 #include "solutions.hpp"
 
 #include <fstream>
+#include <regex>
+#include <utils/String.hpp>
+#include <utils/geometry2d/Point2D.hpp>
 
 namespace aoc_2022_15 {
 
+using namespace utils::geometry2d;
+
 // ---------- Private Methods ----------
+
+std::pair<Point2D, Point2D> parseInputLine(const std::string& line)
+{
+    std::smatch regexResult;
+    constexpr auto Pattern{
+        R"(Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+))"
+    };
+    if (!std::regex_match(line, regexResult, std::regex(Pattern))) {
+        throw std::logic_error("Regex failed in parsing the line");
+    }
+    return std::make_pair(
+        Point2D{ utils::string::toNumber<int32_t>(regexResult[1]),
+                 utils::string::toNumber<int32_t>(regexResult[2]) },
+        Point2D{ utils::string::toNumber<int32_t>(regexResult[3]),
+                 utils::string::toNumber<int32_t>(regexResult[4]) });
+}
 
 // ---------- End of Private Methods ----------
 
