6e0e1c8a8f484aa1a604549c84e04b8d5fd8557c
Hector Fernandez <hecgtor.fernandez@king.com>
2023-04-20 23:01:33 +0200

    .

---
+++ puzzles/2022/20/EXPLANATION.md
@@ -0,0 +1,8 @@
+# Solution explanation for AoC 2022 Day 20
+
+## Part 1
+
+
+The code contains one optimization. Since moving an element by size-1 would leave in the same place, it is sufficient to move each element only by value%(size-1) positions.
+
+## Part 2
--- puzzles/2022/20/sources/solutions.cpp
+++ puzzles/2022/20/sources/solutions.cpp
@@ -1,10 +1,46 @@
 #include "solutions.hpp"
 
 #include <fstream>
+#include <list>
+#include <optional>
+#include <utils/String.hpp>
 
 namespace aoc_2022_20 {
 
 // ---------- Private Methods ----------
+
+std::list<int32_t> parseInput(const std::string& filename)
+{
+    std::ifstream fileStream{filename};
+    std::list<int32_t> outList;
+    std::string line;
+    while (std::getline(fileStream, line)) {
+        outList.emplace_back(utils::string::toNumber<int32_t>(line));
+    }
+    return outList;
+}
+
+uint32_t calculatePositionsToMove(
+    const std::list<int32_t>& numberList, const int32_t value)
+{
+    return static_cast<uint32_t>(std::abs(value)) % (numberList.size() - 1U);
+}
+
+/**
+ * @brief Advances an iterator by the given amount, with wrap-around
+ */
+auto advanceIterator(auto it, const uint32_t amount, auto begin, auto end)
+{
+    auto pos{it};
+    uint32_t counter{amount};
+    while (counter > 0U) {
+        if (pos == end)
+            pos = begin;
+        ++pos;
+        --counter;
+    }
+    return pos;
+}
 
 // ---------- End of Private Methods ----------
 
