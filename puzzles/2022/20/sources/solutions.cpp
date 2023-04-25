75de16f3e32e4327c39b18e28e1413398275f339
Hector Fernandez <hecgtor.fernandez@king.com>
2023-04-25 15:03:26 +0200

    .

--- puzzles/2022/20/sources/solutions.cpp
+++ puzzles/2022/20/sources/solutions.cpp
@@ -3,16 +3,17 @@
 #include <fstream>
 #include <list>
 #include <optional>
+#include <range/v3/all.hpp>
 #include <utils/String.hpp>
 
 namespace aoc_2022_20 {
 
 // ---------- Private Methods ----------
 
-std::list<int32_t> parseInput(std::string const& filename)
+std::vector<int32_t> parseInput(std::string const& filename)
 {
     std::ifstream fileStream{filename};
-    std::list<int32_t> outList;
+    std::vector<int32_t> outList;
     std::string line;
     while (std::getline(fileStream, line)) {
         outList.emplace_back(utils::string::toNumber<int32_t>(line));
@@ -20,26 +21,70 @@ std::list<int32_t> parseInput(std::string const& filename)
     return outList;
 }
 
-uint32_t calculatePositionsToMove(
-    std::list<int32_t> const& numberList, const int32_t value)
+std::size_t
+calculateIndex(std::vector<int32_t> const& list, int32_t const index)
 {
-    return static_cast<uint32_t>(std::abs(value)) % (numberList.size() - 1U);
+    int32_t const divisor{static_cast<int32_t>(list.size() - 1)};
+    if (index < 0 || index > divisor) {
+        // https://www.geeksforgeeks.org/modulus-on-negative-numbers/
+        return static_cast<std::size_t>((index % divisor + divisor) % divisor);
+    }
+    return static_cast<std::size_t>(index);
 }
 
-/**
- * @brief Advances an iterator by the given amount, with wrap-around
- */
-auto advanceIterator(auto it, const uint32_t amount, auto begin, auto end)
+std::size_t calculateNewPosition(
+    std::vector<int32_t> const& list,
+    std::size_t const index,
+    int32_t const movementValue)
 {
-    auto pos{it};
-    uint32_t counter{amount};
-    while (counter > 0U) {
-        if (pos == end)
-            pos = begin;
-        ++pos;
-        --counter;
+    return index + calculateIndex(list, movementValue);
+}
+
+void moveItem(
+    std::vector<int32_t>& list,
+    std::size_t const oldIndex,
+    std::size_t const newIndex)
+{
+    if (oldIndex > newIndex) {
+        std::rotate(
+            std::rend(list) - static_cast<int>(oldIndex) - 1,
+            std::rend(list) - static_cast<int>(oldIndex),
+            std::rend(list) - static_cast<int>(newIndex));
+    } else {
+        std::rotate(
+            std::begin(list) + static_cast<int>(oldIndex),
+            std::begin(list) + static_cast<int>(oldIndex) + 1,
+            std::begin(list) + static_cast<int>(newIndex) + 1);
     }
-    return pos;
+}
+
+int32_t mixNumbers(std::vector<int32_t> const& list)
+{
+    std::vector<int32_t> indexList(list.size());
+    ranges::iota(indexList, 0);
+    // reorder the items
+    for (std::size_t i{0U}; i < list.size(); ++i) {
+        std::size_t const oldIndex{static_cast<std::size_t>(ranges::distance(
+            std::begin(indexList), ranges::find(indexList, i)))};
+        std::size_t newIndex{calculateNewPosition(list, oldIndex, list.at(i))};
+        moveItem(
+            indexList,
+            static_cast<size_t>(oldIndex),
+            static_cast<size_t>(newIndex));
+    }
+    // collect the important values from the list
+    std::size_t zeroIndex{static_cast<std::size_t>(
+        ranges::distance(std::begin(list), ranges::find(list, 0)))};
+    zeroIndex = static_cast<std::size_t>(ranges::distance(
+        std::begin(indexList), ranges::find(indexList, zeroIndex)));
+    int32_t level{0};
+    for (std::size_t i{0U}; i < 3U; ++i) {
+        level += list[static_cast<std::size_t>(
+            indexList[((1000U * i) + 1000U + zeroIndex)])];
+    }
+    return level;
+
+    return 1;
 }
 
 // ---------- End of Private Methods ----------
@@ -48,8 +93,8 @@ auto advanceIterator(auto it, const uint32_t amount, auto begin, auto end)
 
 std::string solvePart1(std::string const& filename)
 {
-    (void)filename;
-    return "";
+    auto numberList{parseInput(filename)};
+    return std::to_string(mixNumbers(numberList));
 }
 
 std::string solvePart2(std::string const& filename)
