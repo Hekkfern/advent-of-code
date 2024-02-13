ad12a296a59fbae01ed55692793926d0f7c04918
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2024-02-13 21:06:47 +0100

    .

--- puzzles/2023/5/sources/RangeMap.cpp
+++ puzzles/2023/5/sources/RangeMap.cpp
@@ -25,11 +25,19 @@ utils::interval::IntervalSet<int64_t> RangeMap::convert(
     utils::interval::Interval<int64_t> const& keyInterval) const noexcept
 {
     utils::interval::IntervalSet<int64_t> result;
+    std::size_t numConvertedItems{0ULL};
+    // check all the sections for matching conversions of the input interval
     for (auto const& section : mSections) {
         auto const value{section.convert(keyInterval)};
         if (value) {
+            numConvertedItems = value->length();
             result.add(*value);
         }
+    }
+    // check if all the input interval was converted
+    if (numConvertedItems <= keyInterval.length()){
+        // fill the remaining with 1-to-1 mapping
+        //TODO
     }
     return result;
 }
