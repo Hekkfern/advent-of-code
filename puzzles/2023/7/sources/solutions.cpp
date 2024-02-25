81f4198e3720cce634f7c673f005c1fa347ac4e9
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2024-02-25 21:14:58 +0100

    day7 intro

---
+++ puzzles/2023/7/sources/solutions.cpp
@@ -0,0 +1,27 @@
+#include "solutions.hpp"
+
+#include <utils/File.hpp>
+
+namespace aoc_2023_7 {
+
+// ---------- Private Methods ----------
+
+// ---------- End of Private Methods ----------
+
+// ---------- Public Methods ----------
+
+std::string solvePart1(std::filesystem::path const& filePath)
+{
+    (void)filePath;
+    return "";
+}
+
+std::string solvePart2(std::filesystem::path const& filePath)
+{
+    (void)filePath;
+    return "";
+}
+
+// ---------- End of Public Methods ----------
+
+} // namespace aoc_2023_7
