5bf54609fe38ca658004df28e529ef54c2c3916c
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2024-02-26 22:24:12 +0100

    .

--- puzzles/2023/8/tests/unittests.cpp
+++ puzzles/2023/8/tests/unittests.cpp
@@ -9,15 +9,29 @@
 #include <utils/File.hpp>
 
 TEST_CASE(
-    "[2023_8] Use example input for Part 1",
+    "[2023_8] Use example input A for Part 1",
     "[2023][2023_8][part1][2023_8_part1]")
 {
-    constexpr auto InputFile{ "input1_test.txt" };
+    constexpr auto InputFile{ "input1a_test.txt" };
 
     std::string const mySolution = aoc_2023_8::solvePart1(InputFile);
     std::string const expectedSolution = utils::file::readFirstLine(
-                                       "solution1_test.txt")
+                                       "solution1a_test.txt")
                                        .value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE(
+    "[2023_8] Use example input B for Part 1",
+    "[2023][2023_8][part1][2023_8_part1]")
+{
+    constexpr auto InputFile{ "input1b_test.txt" };
+
+    std::string const mySolution = aoc_2023_8::solvePart1(InputFile);
+    std::string const expectedSolution = utils::file::readFirstLine(
+                                             "solution1b_test.txt")
+                                             .value();
 
     REQUIRE(mySolution == expectedSolution);
 }
