b3664290376d0eb09e5fd186cfce7d6247fb97a3
Hector Fernandez <hector.fernandez@king.com>
2024-02-28 17:07:17 +0100

    gcd

--- utils/include/utils/Math.hpp
+++ utils/include/utils/Math.hpp
@@ -22,15 +22,15 @@ template <IntegerType T>
     return (dividend % divisor + divisor) % divisor;
 }
 /**
- * @brief
+ * @brief      Computes the "least common multiple" of the selected integers
  *
- * @param m
- * @param n
+ * @param      m     First number.
+ * @param      n     Second number.
  *
- * @tparam M
- * @tparam N
+ * @tparam     M     Type of the first number.
+ * @tparam     N     Type of the second number.
  *
- * @return constexpr auto
+ * @return     Result of the operation.
  */
 template <NumericType M, NumericType N>
 [[nodiscard]] constexpr auto lcm(M const& m, N const& n)
@@ -38,20 +38,52 @@ template <NumericType M, NumericType N>
     return std::lcm(m, n);
 }
 /**
- * @brief
+ * @brief      Computes the "least common multiple" of the selected integers
  *
- * @param first
- * @param rest
+ * @param      first  First number.
+ * @param      rest   Other numbers.
  *
- * @tparam M
- * @tparam Rest
+ * @tparam     M      Type of the first number.
+ * @tparam     Rest   Type of the other numbers.
  *
- * @return constexpr auto
+ * @return     Result of the operation.
  */
 template <NumericType M, NumericType... Rest>
 [[nodiscard]] constexpr auto lcm(M const& first, Rest const&... rest)
 {
     return std::lcm(first, lcm(rest...));
+}
+/**
+ * @brief      Computes the "greatest common divisor" of the selected integers
+ *
+ * @param[in]  m     First number.
+ * @param[in]  n     Second number.
+ *
+ * @tparam     M     Type of the first number.
+ * @tparam     N     Type of the second number.
+ *
+ * @return     Result of the operation.
+ */
+template <NumericType M, NumericType N>
+[[nodiscard]] constexpr auto gcd(M const& m, N const& n)
+{
+    return std::gcd(m, n);
+}
+/**
+ * @brief      Computes the "greatest common divisor" of the selected integers
+ *
+ * @param[in]  first  First number.
+ * @param[in]  rest   Other numbers.
+ *
+ * @tparam     M      Type of the first number.
+ * @tparam     Rest   Type of the other numbers.
+ *
+ * @return     Result of the operation.
+ */
+template <NumericType M, NumericType... Rest>
+[[nodiscard]] constexpr auto gcd(M const& first, Rest const&... rest)
+{
+    return std::gcd(first, gcd(rest...));
 }
 
 } // namespace utils::math
