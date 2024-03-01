3f097dcce9572f9482b9dfbb9df457a45d6c2e50
Hector Fernandez <hector.fernandez@king.com>
2024-03-01 10:08:36 +0100

    .

---
+++ puzzles/2023/10/sources/PipeType.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace aoc_2023_10 {
+
+enum class PipeType {
+    None,
+    Horizontal,
+    Vertical,
+    NorthEast,
+    NorthWest,
+    SouthWest,
+    SouthEast,
+    StartingPosition
+};
+
+} //
