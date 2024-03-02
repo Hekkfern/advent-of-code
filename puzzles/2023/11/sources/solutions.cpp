16b9665204f743939098ada0e9186c3c93f9a4ee
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2024-03-02 20:53:33 +0100

    .

--- puzzles/2023/11/sources/solutions.cpp
+++ puzzles/2023/11/sources/solutions.cpp
@@ -1,6 +1,8 @@
 #include "solutions.hpp"
 
 #include "GalaxyMap.hpp"
+#include <range/v3/algorithm/all_of.hpp>
+#include <range/v3/view/enumerate.hpp>
 #include <utils/File.hpp>
 
 namespace aoc_2023_11 {
@@ -11,12 +13,25 @@ GalaxyMap parseInput(std::filesystem::path const& filePath)
 {
     GalaxyMap galaxyMap;
 
+    // parse file and check for empty rows
     utils::file::parseAndIterateWithIndex(
         filePath,
         [&galaxyMap](
             std::size_t const rowIndex, std::string_view const line) -> void {
-            // TODO
+            std::size_t galaxyCount{0ULL};
+            for (auto const [colIndex, c] : line | ranges::views::enumerate) {
+                if (c == '#') {
+                    galaxyMap.mGalaxies.emplace_back(colIndex, rowIndex);
+                    ++galaxyCount;
+                }
+            }
+            // check if the row was empty
+            if (galaxyCount == 0ULL) {
+                galaxyMap.mEmptyRows.emplace_back(rowIndex);
+            }
         });
+    // check for empty columns
+    //TODO
 
     return galaxyMap;
 }
