e117fe401994a35c63d18c53a4be9d94d6ea5175
Hector Fernandez <hector.fernandez.carmona@gmail.com>
2024-03-03 19:17:56 +0100

    .

--- utils/include/utils/Combinations.hpp
+++ utils/include/utils/Combinations.hpp
@@ -71,21 +71,4 @@ void forEachCombination(
         container, K, std::forward<Function>(function), combination, 0);
 }
 
-/**
- * @brief Generates all the possible combinations from a set of collections and
- * applies a function to each combination.
- *
- * https://www.fluentcpp.com/2022/03/18/how-to-generate-all-the-combinations-from-several-collections/
- *
- * @tparam Function
- * @tparam Ranges
- *
- * @param function
- * @param ranges
- */
-template <typename Function, typename... Ranges>
-void cartesianProduct(Function function, Ranges const&... ranges)
-{
-}
-
 } // namespace utils::combinations
