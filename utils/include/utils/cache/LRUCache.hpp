e0560398f8d80af546d32395ed20f7c0ee44dc56
Hector Fernandez <hector.fernandez@king.com>
2024-03-08 16:56:12 +0100

    .

--- utils/include/utils/cache/LRUCache.hpp
+++ utils/include/utils/cache/LRUCache.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
-#include <unordered_map>
 #include <list>
+#include <unordered_map>
 
 namespace utils::cache {
 
+template <typename Key, typename Value>
 class LRUCache {
 public:
-    LRUCache(int capacity) : capacity(capacity) {}
+    LRUCache(std::size_t const capacity)
+        : mCapacity(capacity)
+    {
+    }
 
-    int get(int key) {
+    int get(int key)
+    {
         if (cacheMap.find(key) == cacheMap.end()) {
             return -1;
         }
@@ -20,14 +25,15 @@ public:
         return cacheMap[key]->second;
     }
 
-    void put(int key, int value) {
+    void put(int key, int value)
+    {
         if (cacheMap.find(key) != cacheMap.end()) {
             // Update the value and move the element to the front
             cacheMap[key]->second = value;
             cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
         } else {
             // Insert the new element
-            if (cacheList.size() == capacity) {
+            if (cacheList.size() == mCapacity) {
                 int lastKey = cacheList.back().first;
                 cacheMap.erase(lastKey);
                 cacheList.pop_back();
@@ -38,8 +44,8 @@ public:
         }
     }
 
-    private:
-    int capacity;
+private:
+    std::size_t mCapacity;
     std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cacheMap;
     std::list<std::pair<int, int>> cacheList;
 };
