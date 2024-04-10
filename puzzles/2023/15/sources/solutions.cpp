eb6f5eb6fdca2a22143f4b7f61623f9c86a74b32
Hector Fernandez <hector.fernandez@king.com>
2024-04-10 21:22:27 +0200

    .

--- puzzles/2023/15/sources/solutions.cpp
+++ puzzles/2023/15/sources/solutions.cpp
@@ -1,12 +1,25 @@
 #include "solutions.hpp"
 
 #include <fstream>
-#include <range/v3/
 #include <range/v3/algorithm/fold_left.hpp>
+#include <string_view>
 
 namespace aoc_2023_15 {
 
 // ---------- Private Methods ----------
+
+uint64_t calculateHash(uint64_t initialValue, char const c)
+{
+    return (initialValue + static_cast<uint64_t>(c)) * 17ULL % 256ULL;
+}
+
+uint64_t calculateHash(std::string_view const str)
+{
+    return ranges::fold_left(
+        str, 0ULL, [](uint64_t const initialValue, char const c) {
+            return calculateHash(initialValue, c);
+        });
+}
 
 // ---------- End of Private Methods ----------
 
