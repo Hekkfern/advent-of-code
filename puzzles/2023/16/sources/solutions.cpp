71cd6b4ba4220d8fc5b5000b7880257400413de0
Hector Fernandez <hector.fernandez@king.com>
2024-04-14 09:04:45 +0200

    ,

--- puzzles/2023/16/sources/solutions.cpp
+++ puzzles/2023/16/sources/solutions.cpp
@@ -1,10 +1,25 @@
 #include "solutions.hpp"
 
 #include <utils/File.hpp>
+#include <utils/geometry2d/Grid2D.hpp>
 
 namespace aoc_2023_16 {
 
 // ---------- Private Methods ----------
+
+/**
+ * @brief      Parses the whole input file and generates a flatten grid.
+ *
+ * @param[in]  filePath  The path to the input file.
+ *
+ * @return     Parsed data.
+ */
+utils::geometry2d::Grid2D<char>
+parseInput(std::filesystem::path const& filePath)
+{
+    auto const data{*utils::file::readMatrixOfChars(filePath)};
+    return utils::geometry2d::Grid2D<char>{data};
+}
 
 // ---------- End of Private Methods ----------
 
