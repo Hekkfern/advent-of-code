43c8e9af95b7dc5719cc43d8eb6f9773904cc66b
Hector Fernandez <hector.fernandez@king.com>
2024-05-10 12:16:00 +0200

    improve concepts

--- utils/include/utils/Concepts.hpp
+++ utils/include/utils/Concepts.hpp
@@ -10,41 +10,7 @@
  * @tparam     T     Type to validate the concept against.
  */
 template <typename T>
-concept NumericType = requires(T param)
-{
-    requires std::integral<T> || std::floating_point<T>;
-    requires !std::is_same_v<bool, T>;
-    requires std::is_arithmetic_v<decltype(param + 1)>;
-    requires !std::is_pointer_v<T>;
-};
-
-/**
- * @brief      Defines a type which is a unsigned integer type of any kind.
- *
- * @tparam     T     Type to validate the concept against.
- */
-template <typename T>
-concept UnsignedIntegerType = requires(T param)
-{
-    requires std::unsigned_integral<T>;
-    requires !std::is_same_v<bool, T>;
-    requires std::is_arithmetic_v<decltype(param + 1)>;
-    requires !std::is_pointer_v<T>;
-};
-
-/**
- * @brief      Defines a type which is a signed integer type of any kind.
- *
- * @tparam     T     Type to validate the concept against.
- */
-template <typename T>
-concept SignedIntegerType = requires(T param)
-{
-    requires std::signed_integral<T>;
-    requires !std::is_same_v<bool, T>;
-    requires std::is_arithmetic_v<decltype(param + 1)>;
-    requires !std::is_pointer_v<T>;
-};
+concept NumericType = std::is_arithmetic_v<T> && !std::is_same_v<bool, T>;
 
 /**
  * @brief      Defines a type which is a integer type of any kind.
@@ -52,13 +18,23 @@ concept SignedIntegerType = requires(T param)
  * @tparam     T     Type to validate the concept against.
  */
 template <typename T>
-concept IntegerType = requires(T param)
-{
-    requires std::integral<T>;
-    requires !std::is_same_v<bool, T>;
-    requires std::is_arithmetic_v<decltype(param + 1)>;
-    requires !std::is_pointer_v<T>;
-};
+concept IntegerType = NumericType<T> && std::integral<T>;
+
+/**
+ * @brief      Defines a type which is a unsigned integer type of any kind.
+ *
+ * @tparam     T     Type to validate the concept against.
+ */
+template <typename T>
+concept UnsignedIntegerType = NumericType<T> && std::unsigned_integral<T>;
+
+/**
+ * @brief      Defines a type which is a signed integer type of any kind.
+ *
+ * @tparam     T     Type to validate the concept against.
+ */
+template <typename T>
+concept SignedIntegerType = NumericType<T> && std::signed_integral<T>;
 
 /**
  * @brief
@@ -66,11 +42,10 @@ concept IntegerType = requires(T param)
  * @tparam T
  */
 template <typename T>
-concept Streamable = requires(std::ostream& os, T value)
-{
+concept Streamable = requires(std::ostream& os, T value) {
     {
         os << value
-        } -> std::convertible_to<std::ostream&>;
+    } -> std::convertible_to<std::ostream&>;
 };
 
 /**
@@ -79,9 +54,8 @@ concept Streamable = requires(std::ostream& os, T value)
  * @tparam DataType
  */
 template <typename T>
-concept Hashable = requires(T value)
-{
+concept Hashable = requires(T value) {
     {
         std::hash<T>{}(value)
-        } -> std::convertible_to<std::size_t>;
+    } -> std::convertible_to<std::size_t>;
 };
--- utils/tests/ConceptsTests.cpp
+++ utils/tests/ConceptsTests.cpp
@@ -7,12 +7,81 @@ TEST_CASE("[Concepts] NumericType", "[utils][Concepts]")
     STATIC_CHECK(not NumericType<std::string>);
     STATIC_CHECK(not NumericType<bool>);
     STATIC_CHECK(not NumericType<std::vector<int>>);
+    STATIC_CHECK(not NumericType<int8_t*>);
+    STATIC_CHECK(not NumericType<char*>);
+    STATIC_CHECK(not NumericType<std::byte>);
+    STATIC_CHECK(not NumericType<std::unique_ptr<int32_t>>);
+    STATIC_CHECK(NumericType<char>);
     STATIC_CHECK(NumericType<int8_t>);
     STATIC_CHECK(NumericType<int32_t>);
     STATIC_CHECK(NumericType<int64_t>);
     STATIC_CHECK(NumericType<uint8_t>);
     STATIC_CHECK(NumericType<uint32_t>);
     STATIC_CHECK(NumericType<uint64_t>);
+    STATIC_CHECK(NumericType<std::size_t>);
     STATIC_CHECK(NumericType<float_t>);
     STATIC_CHECK(NumericType<double_t>);
+}
+
+TEST_CASE("[Concepts] IntegerType", "[utils][Concepts]")
+{
+    STATIC_CHECK(not IntegerType<std::string>);
+    STATIC_CHECK(not IntegerType<bool>);
+    STATIC_CHECK(not IntegerType<std::vector<int>>);
+    STATIC_CHECK(not IntegerType<int8_t*>);
+    STATIC_CHECK(not IntegerType<char*>);
+    STATIC_CHECK(not IntegerType<std::byte>);
+    STATIC_CHECK(not IntegerType<std::unique_ptr<int32_t>>);
+    STATIC_CHECK(IntegerType<char>);
+    STATIC_CHECK(IntegerType<int8_t>);
+    STATIC_CHECK(IntegerType<int32_t>);
+    STATIC_CHECK(IntegerType<int64_t>);
+    STATIC_CHECK(IntegerType<uint8_t>);
+    STATIC_CHECK(IntegerType<uint32_t>);
+    STATIC_CHECK(IntegerType<uint64_t>);
+    STATIC_CHECK(IntegerType<std::size_t>);
+    STATIC_CHECK(not IntegerType<float_t>);
+    STATIC_CHECK(not IntegerType<double_t>);
+}
+
+TEST_CASE("[Concepts] UnsignedIntegerType", "[utils][Concepts]")
+{
+    STATIC_CHECK(not UnsignedIntegerType<std::string>);
+    STATIC_CHECK(not UnsignedIntegerType<bool>);
+    STATIC_CHECK(not UnsignedIntegerType<std::vector<int>>);
+    STATIC_CHECK(not UnsignedIntegerType<int8_t*>);
+    STATIC_CHECK(not UnsignedIntegerType<char*>);
+    STATIC_CHECK(not UnsignedIntegerType<std::byte>);
+    STATIC_CHECK(not UnsignedIntegerType<std::unique_ptr<int32_t>>);
+    STATIC_CHECK(not UnsignedIntegerType<char>);
+    STATIC_CHECK(not UnsignedIntegerType<int8_t>);
+    STATIC_CHECK(not UnsignedIntegerType<int32_t>);
+    STATIC_CHECK(not UnsignedIntegerType<int64_t>);
+    STATIC_CHECK(UnsignedIntegerType<uint8_t>);
+    STATIC_CHECK(UnsignedIntegerType<uint32_t>);
+    STATIC_CHECK(UnsignedIntegerType<uint64_t>);
+    STATIC_CHECK(UnsignedIntegerType<std::size_t>);
+    STATIC_CHECK(not UnsignedIntegerType<float_t>);
+    STATIC_CHECK(not UnsignedIntegerType<double_t>);
+}
+
+TEST_CASE("[Concepts] SignedIntegerType", "[utils][Concepts]")
+{
+    STATIC_CHECK(not SignedIntegerType<std::string>);
+    STATIC_CHECK(not SignedIntegerType<bool>);
+    STATIC_CHECK(not SignedIntegerType<std::vector<int>>);
+    STATIC_CHECK(not SignedIntegerType<int8_t*>);
+    STATIC_CHECK(not SignedIntegerType<char*>);
+    STATIC_CHECK(not SignedIntegerType<std::byte>);
+    STATIC_CHECK(not SignedIntegerType<std::unique_ptr<int32_t>>);
+    STATIC_CHECK(SignedIntegerType<char>);
+    STATIC_CHECK(SignedIntegerType<int8_t>);
+    STATIC_CHECK(SignedIntegerType<int32_t>);
+    STATIC_CHECK(SignedIntegerType<int64_t>);
+    STATIC_CHECK(not SignedIntegerType<uint8_t>);
+    STATIC_CHECK(not SignedIntegerType<uint32_t>);
+    STATIC_CHECK(not SignedIntegerType<uint64_t>);
+    STATIC_CHECK(not SignedIntegerType<std::size_t>);
+    STATIC_CHECK(not SignedIntegerType<float_t>);
+    STATIC_CHECK(not SignedIntegerType<double_t>);
 }
