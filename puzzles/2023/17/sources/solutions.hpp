74f0edd32b9b1f098c650c2c3575d54e4a855841
Hector Fernandez <hector.fernandez@king.com>
2024-06-12 23:16:27 +0200

    day17

---
+++ puzzles/2023/17/sources/solutions.hpp
@@ -0,0 +1,15 @@
+//**********************************************************************
+// GENERATED CODE - DO NOT MODIFY
+//**********************************************************************
+
+#pragma once
+
+#include <filesystem>
+#include <string>
+
+namespace aoc_2023_17 {
+
+std::string solvePart1(std::filesystem::path const& filePath);
+std::string solvePart2(std::filesystem::path const& filePath);
+
+}
---
+++ puzzles/2023/17/tests/unittests.cpp
@@ -0,0 +1,65 @@
+//**********************************************************************
+// GENERATED CODE - DO NOT MODIFY
+//**********************************************************************
+
+#include "../sources/solutions.hpp"
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <string>
+#include <utils/File.hpp>
+
+TEST_CASE(
+    "[2023_17] Use example input for Part 1",
+    "[2023][2023_17][part1][2023_17_part1]")
+{
+    constexpr auto InputFile{ "input1_test.txt" };
+
+    std::string const mySolution = aoc_2023_17::solvePart1(InputFile);
+    std::string const expectedSolution = utils::file::readFirstLine(
+                                       "solution1_test.txt")
+                                       .value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE(
+    "[2023_17] Use example input for Part 2",
+    "[2023][2023_17][part2][2023_17_part2]")
+{
+    constexpr auto InputFile{ "input2_test.txt" };
+
+    std::string const mySolution = aoc_2023_17::solvePart2(InputFile);
+    std::string const expectedSolution = utils::file::readFirstLine(
+                                       "solution2_test.txt")
+                                       .value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE(
+    "[2023_17] Use real input for Part 1",
+    "[2023][2023_17][part1][2023_17_part1]")
+{
+    constexpr auto InputFile{ "input.txt" };
+
+    std::string const mySolution = aoc_2023_17::solvePart1(InputFile);
+    std::string const expectedSolution = utils::file::readFirstLine(
+                                       "solution1.txt")
+                                       .value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
+
+TEST_CASE(
+    "[2023_17] Use real input for Part 2",
+    "[2023][2023_17][part1][2023_17_part2]")
+{
+    constexpr auto InputFile{ "input.txt" };
+
+    std::string const mySolution = aoc_2023_17::solvePart2(InputFile);
+    std::string const expectedSolution = utils::file::readFirstLine(
+                                       "solution2.txt")
+                                       .value();
+
+    REQUIRE(mySolution == expectedSolution);
+}
