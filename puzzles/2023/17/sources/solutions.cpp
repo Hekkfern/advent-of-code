b2206e84d107f84ea10a8bfd9a3b59f2b89f7d39
Hector Fernandez <hector.fernandez@king.com>
2024-09-27 17:08:02 +0200

    .

--- puzzles/2023/17/sources/solutions.cpp
+++ puzzles/2023/17/sources/solutions.cpp
@@ -19,6 +19,13 @@ using Coord = Coordinate2D<std::size_t>;
 
 using HeatLossGrid = Grid2D<uint8_t>;
 
+/**
+ * @brief
+ *
+ * @param[in] filePath
+ *
+ * @return
+ */
 HeatLossGrid parseInput(std::filesystem::path const& filePath)
 {
     auto const data{*utils::file::readMatrixOfDigits(filePath)};
@@ -32,29 +39,47 @@ struct CompareStates {
     }
 };
 
-std::vector<std::pair<Coord, Direction2D>>
-getNextSteps(HeatLossGrid const& grid, State const& currentState)
+/**
+ * @brief
+ *
+ * @param[in] grid
+ * @param[in] currentState
+ * @param[in] minSteps
+ *
+ * @return
+ */
+std::vector<std::pair<Coord, Direction2D>> getNextSteps(
+    HeatLossGrid const& grid,
+    State const& currentState,
+    uint32_t const minSteps,
+    uint32_t const maxSteps)
 {
     std::vector<std::pair<Coord, Direction2D>> nextSteps;
     nextSteps.reserve(3);
-    /* straight movement */
-    auto straightDirection{currentState.direction.goStraight()};
-    auto straightMovement{grid.move(currentState.position, straightDirection)};
-    if (straightMovement) {
-        nextSteps.emplace_back(*straightMovement, straightDirection);
+    if (currentState.steps < maxSteps) {
+        /* straight movement */
+        auto straightDirection{currentState.direction.goStraight()};
+        auto straightMovement{
+            grid.move(currentState.position, straightDirection)};
+        if (straightMovement) {
+            nextSteps.emplace_back(*straightMovement, straightDirection);
+        }
     }
-    /* turn-left movement */
-    auto turnLeftDirection{currentState.direction.turnLeft()};
-    auto turnLeftMovement{grid.move(currentState.position, turnLeftDirection)};
-    if (turnLeftMovement) {
-        nextSteps.emplace_back(*turnLeftMovement, turnLeftDirection);
-    }
-    /* turn-right movement */
-    auto turnRightDirection{currentState.direction.turnRight()};
-    auto turnRightMovement{
-        grid.move(currentState.position, turnRightDirection)};
-    if (turnRightMovement) {
-        nextSteps.emplace_back(*turnRightMovement, turnRightDirection);
+    if (currentState.steps >= minSteps) {
+        /* turn-left movement */
+        auto turnLeftDirection{currentState.direction.turnLeft()};
+        auto turnLeftMovement{
+            grid.move(currentState.position, turnLeftDirection)};
+        if (turnLeftMovement) {
+            nextSteps.emplace_back(*turnLeftMovement, turnLeftDirection);
+        }
+        /* turn-right movement */
+        auto turnRightDirection{currentState.direction.turnRight()};
+        auto turnRightMovement{
+            grid.move(currentState.position, turnRightDirection)};
+        if (turnRightMovement) {
+            nextSteps.emplace_back(*turnRightMovement, turnRightDirection);
+        }
     }
     return nextSteps;
 }
@@ -66,9 +91,7 @@ uint32_t getLeastHeatLoss(
     Coord const origin{0ULL, grid.getHeight() - 1ULL};
     State const startingThroughEast{origin, Direction2D::Right, 0, 0};
     State const startingThroughSouth{origin, Direction2D::Down, 0, 0};
-    std::priority_queue<State, std::vector<State>, CompareStates> pq{};
-    pq.push(startingThroughEast);
-    pq.push(startingThroughSouth);
+    std::priority_queue<State, std::vector<State>, CompareStates> pq;
     std::unordered_map<Node, uint32_t> best;
 
     // Helper that adds a given state to the queue if it is better than our best
@@ -91,29 +114,22 @@ uint32_t getLeastHeatLoss(
         /* If we are at our destination, report the heat_loss, because of the
          * priority queue we have a guarantee this is the minimum value */
         if (current.position == destination) {
+            if (current.steps < minSteps) {
+                continue;
+            }
             return current.heatLoss;
         }
         pq.pop();
 
         std::vector<std::pair<Coord, Direction2D>> nextSteps{
-            getNextSteps(grid, current)};
+            getNextSteps(grid, current, minSteps, maxSteps)};
 
         for (auto const& nextStep : nextSteps) {
-            if (current.steps > (maxSteps - 1)
-                && current.direction == nextStep.second) {
-                continue;
-            }
-            if (current.steps < minSteps
-                && current.direction != nextStep.second) {
-                continue;
-            }
-
             State nextState{
                 nextStep.first,
                 nextStep.second,
                 current.direction == nextStep.second ? current.steps + 1U : 1U,
                 current.heatLoss + grid.at(nextStep.first)};
-
             push_if_better(nextState);
         }
     }
