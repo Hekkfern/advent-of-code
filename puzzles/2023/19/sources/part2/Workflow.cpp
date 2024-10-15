7976db494166490221fabac743fa008dda7bfd44
Hector Fernandez <hector.fernandez@king.com>
2024-10-15 09:09:56 +0200

    .

--- puzzles/2023/19/sources/part2/Workflow.cpp
+++ puzzles/2023/19/sources/part2/Workflow.cpp
@@ -9,25 +9,8 @@ Workflow::Workflow(std::string_view const name)
 
 void Workflow::addRule(Rule const& rule) { mRules.emplace_back(rule); }
 
-Workflow::RunResult Workflow::run(Part const& part) const noexcept
-{
-    for (auto const& rule : mRules) {
-        Rule::RunResult const ruleResult{rule.run(part)};
-        switch (ruleResult.result) {
-        case Rule::Result::Accepted:
-            return RunResult{Result::Accepted};
-        case Rule::Result::Rejected:
-            return RunResult{Result::Rejected};
-        case Rule::Result::Skip:
-            continue;
-        case Rule::Result::GoTo:
-            return RunResult{Result::GoTo, ruleResult.goToWorkflow};
-        }
-    }
-    /* impossible */
-    return RunResult{Result::Rejected};
-}
-
 std::string Workflow::getName() const noexcept { return mName; }
+
+std::vector<Rule> const& Workflow::getRules() const noexcept { return mRules; }
 
 } // namespace aoc_2023_19::part2
--- puzzles/2023/19/sources/part2/Workflow.h
+++ puzzles/2023/19/sources/part2/Workflow.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Part.h"
 #include "Rule.h"
 #include <string>
 #include <unordered_map>
@@ -9,17 +8,6 @@ namespace aoc_2023_19::part2 {
 
 class Workflow {
 public:
-    /**
-     * @brief      Enum describing the result of @ref run method execution.
-     */
-    enum class Result { Accepted, Rejected, GoTo };
-    /**
-     * @brief      Returned data structure by @ref run method.
-     */
-    struct RunResult {
-        Result result;
-        std::string goToWorkflow{""};
-    };
     /**
      * @brief      Constructs a new instance.
      *
@@ -33,20 +21,17 @@ public:
      */
     void addRule(Rule const& rule);
     /**
-     * @brief      Processes the part against the list of rules forming this
-     *             workflow.
-     *
-     * @param[in]  part  The part to process.
-     *
-     * @return     The result.
-     */
-    [[nodiscard]] RunResult run(Part const& part) const noexcept;
-    /**
      * @brief      Gets the identifier of the workflow.
      *
      * @return     The identifier.
      */
     [[nodiscard]] std::string getName() const noexcept;
+    /**
+     * @brief ???
+     *
+     * @return ???
+     */
+    [[nodiscard]] std::vector<Rule> const& getRules() const noexcept;
 
 private:
     /**
